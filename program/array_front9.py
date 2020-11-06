6b5bd1c84c0235fcf3d3dca588cf6989d42a4411
Hannah L-S <hlistersims@gmail.com>
2020-11-06 09:20:32 +0000

    more coding bat exercises

---
+++ program/array_front9.py
@@ -0,0 +1,9 @@
+def array_font9( nums ) :
+    end = len(nums)
+    if end > 4 :
+        end = 4
+    
+    for i in range( end ):
+        if num[i] == 9 :
+            return True
+    return False
